431b564daf71873a829c1b571428740e589884bf
Ravindar Cheganti <ravindar_cheganti@epam.com>
2019-05-29 20:19:58 +0530

    Urls update in the server

--- newsServer.js
+++ newsServer.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-const getRouter = require('../get');
-const postRouter = require('../post');
-const putRouter = require('../put');
-const deleteRouter = require('../delete');
+const getRouter = require('./app/news/get');
+const postRouter = require('./app/news/post');
+const putRouter = require('./app/news/put');
+const deleteRouter = require('./app/news/delete');
 
 app.use(express.json());
-app.use('/api/news', getRouter)
-app.use('/api/news', postRouter)
-app.use('/api/news', putRouter)
-app.use('/api/news', deleteRouter)
+app.use('/api/news', getRouter);
+app.use('/api/news', postRouter);
+app.use('/api/news', putRouter);
+app.use('/api/news', deleteRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`we are listening on ${port}`));
