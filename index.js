062eb38167d4afa9da27e66e5300c86df4d5b70c
Ravindar Cheganti <ravindar_cheganti@epam.com>
2019-06-03 19:27:38 +0530

    updated code

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ app.use(express.json());
 
 routes(app);
 
-  app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.status(404).send('Incorrect API Endpoint!');
-  });
+});
 
-  app.use((err,req, res, next) =>{
-    res.status(500).send('Internal server error occured');
-  });
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err.status
+    });
+});
 
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`we are listening on ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`we are listening on ${port}`));
