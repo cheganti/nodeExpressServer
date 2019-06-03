5a90f5256a6347ef00a76e81fe331a041b8e3578
Ravindar Cheganti <ravindar_cheganti@epam.com>
2019-06-03 19:31:25 +0530

    removed comments

--- src/interfaces/http/router.js
+++ src/interfaces/http/router.js
@@ -3,6 +3,4 @@ const controller = require('./utils/create_controller')
 module.exports = (appRouter) => {
   
     appRouter.use('/api/news/', controller('news'))
-
-    //appRouter.use('/api/articles/', controller('articles'))
 }
