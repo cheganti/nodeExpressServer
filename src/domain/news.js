62b106ce51cf94ecf64376e7567f070b6d17a13a
Ravindar Cheganti <ravindar_cheganti@epam.com>
2019-06-14 17:45:21 +0530

    removed unused files

--- src/domain/news.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const newsSchema = new Schema({
-  _id: Schema.Types.ObjectId,
-  name: String
-});
-
-module.exports = mongoose.model('News', newsSchema);
--- src/infra/repositories/index.js
+++
@@ -1,9 +0,0 @@
-const News = require('./news')
-
-module.exports = ({ database }) => {
-  const newsModel = database.models.News
-
-  return {
-    NewsRepository: News({ model: newsModel })
-  }
-}
